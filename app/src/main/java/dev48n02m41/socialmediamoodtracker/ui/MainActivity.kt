83881ba1cd0b408e4e7a86fd1aeb45d608604523
48n02m41 <54042201+48n02m41@users.noreply.github.com>
2021-03-20 02:45:57 +0000

    Login working.

--- app/src/main/java/dev48n02m41/socialmediamoodtracker/ui/MainActivity.kt
+++ app/src/main/java/dev48n02m41/socialmediamoodtracker/ui/MainActivity.kt
@@ -8,12 +8,24 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.auth0.android.Auth0
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.authentication.AuthenticationAPIClient
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.management.ManagementException
+import com.auth0.android.management.UsersAPIClient
+import com.auth0.android.result.Credentials
+import com.auth0.android.result.UserProfile
+import com.auth0.android.callback.Callback
 import dev48n02m41.socialmediamoodtracker.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 private lateinit var textViewHeader: TextView
 private lateinit var btnAsk: Button
 private lateinit var btnList: Button
+private lateinit var account: Auth0
+private lateinit var client: String
+private lateinit var domain: String
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +34,14 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
         handleUI()
         animate()
+
+        client = getString(R.string.com_auth0_client_id)
+        domain = getString(R.string.com_auth0_domain)
+
+        // Auth0 account object
+        account = Auth0(
+            client, domain
+        )
     }
 
     private fun handleUI() {
@@ -68,6 +88,64 @@ class MainActivity : AppCompatActivity() {
             repeatMode = ValueAnimator.REVERSE
             start()
         }
+    }
+
+    private fun loginWithBrowser() {
+        // Setup the WebAuthProvider, using the custom scheme and scope.
+
+        WebAuthProvider.login(account)
+            .withScheme("app")
+            .withScope("openid profile email")
+            // Launch the authentication passing the callback where the results will be received
+            .start(this, object : Callback<Credentials, AuthenticationException> {
+                // Called when there is an authentication failure
+                override fun onFailure(exception: AuthenticationException) {
+                    // Something went wrong!
+                }
+
+                // Called when authentication completed successfully
+                override fun onSuccess(credentials: Credentials) {
+                    // Get the access token from the credentials object.
+                    // This can be used to call APIs
+                    val accessToken = credentials.accessToken
+                }
+            })
+    }
+
+    private fun logout() {
+        WebAuthProvider.logout(account)
+            .withScheme("app")
+            .start(this, object : Callback<Void?, AuthenticationException> {
+                override fun onSuccess(payload: Void?) {
+                    // The user has been logged out!
+                }
+
+                override fun onFailure(error: AuthenticationException) {
+                    // Something went wrong!
+                }
+            })
+    }
+
+    private fun showUserProfile(accessToken: String) {
+        var client = AuthenticationAPIClient(account)
+
+        // With the access token, call `userInfo` and get the profile from Auth0.
+        client.userInfo(accessToken)
+            .start(object : Callback<UserProfile, AuthenticationException> {
+                override fun onFailure(exception: AuthenticationException) {
+                    // Something went wrong!
+                }
+
+                override fun onSuccess(profile: UserProfile) {
+                    // We have the user's profile!
+                    val email = profile.email
+                    val name = profile.name
+                }
+            })
+    }
+
+    fun login(view: View) {
+        loginWithBrowser()
     }
 
     fun openAskActivity(view: View) {
