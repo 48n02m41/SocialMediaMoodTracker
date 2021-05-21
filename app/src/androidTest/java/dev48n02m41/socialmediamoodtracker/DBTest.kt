56edaab2c7e718c9fa674723ebd529b8b6d68ebc
48n02m41 <54042201+48n02m41@users.noreply.github.com>
2021-05-21 01:29:28 +0100

    Tests for database read write.

---
+++ app/src/androidTest/java/dev48n02m41/socialmediamoodtracker/DBTest.kt
@@ -0,0 +1,56 @@
+package dev48n02m41.socialmediamoodtracker
+
+import android.content.Context
+import androidx.lifecycle.asLiveData
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import dev48n02m41.socialmediamoodtracker.data.AppDatabase
+import dev48n02m41.socialmediamoodtracker.data.dao.APIDiaryEntryDao
+import dev48n02m41.socialmediamoodtracker.data.dao.DiaryEntryDao
+import dev48n02m41.socialmediamoodtracker.data.entities.DiaryEntryEntity
+import org.hamcrest.CoreMatchers.*
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class DBTest {
+    private lateinit var tempAppDatabase: AppDatabase
+    private lateinit var diaryEntryDao: DiaryEntryDao
+    private lateinit var apiDiaryEntryDao: APIDiaryEntryDao
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        tempAppDatabase = Room
+            .inMemoryDatabaseBuilder(context, AppDatabase::class.java)
+            .build()
+
+        diaryEntryDao = tempAppDatabase.diaryEntryDao()
+        apiDiaryEntryDao = tempAppDatabase.apiDiaryEntryDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        tempAppDatabase.close()
+    }
+
+    @Test
+    fun writeAndReadNewDBData() {
+        val newEntry = DiaryEntryEntity ("Twitter", 2, 4)
+
+        diaryEntryDao.insertTest(newEntry)
+
+        val findByNetwork = diaryEntryDao.getBySocialNetworkTest("Twitter")
+        assertThat(findByNetwork[0], equalTo(newEntry))
+        assertThat(findByNetwork[0].afterMoodRating, equalTo(4))
+        assertThat(findByNetwork[0].beforeMoodRating, equalTo(2))
+        assertThat(findByNetwork[0].id, notNullValue())
+    }
+
+}
--- app/src/main/java/dev48n02m41/socialmediamoodtracker/data/dao/DiaryEntryDao.kt
+++ app/src/main/java/dev48n02m41/socialmediamoodtracker/data/dao/DiaryEntryDao.kt
@@ -16,11 +16,17 @@ interface DiaryEntryDao {
     @Query("SELECT * FROM diary_entry_table WHERE social_network = :socialNetworkIn ORDER BY id DESC")
     abstract fun getBySocialNetwork(socialNetworkIn: String): Flow<List<DiaryEntryEntity>>
 
+    @Query("SELECT * FROM diary_entry_table WHERE social_network = :socialNetworkIn ORDER BY id DESC")
+    fun getBySocialNetworkTest(socialNetworkIn: String): List<DiaryEntryEntity>
+
     fun getAllBySocialNetworkFlow(socialNetworkIn: String) = getBySocialNetwork(socialNetworkIn).distinctUntilChanged()
 
     // Insert / update
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertOne(vararg objectIn: DiaryEntryEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertTest(vararg objectIn: DiaryEntryEntity)
 
     @Update
     suspend fun updateOne(vararg objectIn: DiaryEntryEntity)
